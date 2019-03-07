62241d45e46f8cf509f28bf61d3968bcdb43dd1d
Diana Costa <dianasofia.costa@sapo.pt>
2019-03-07 14:18:48 +0000

    database connection & DB consulting examples

--- database/database.py
+++ database/database.py
@@ -1,15 +1,28 @@
 '''
-Requisitos para isto funcionar: Mongod e pymongo instalado
+- Correr mongod
+- Correr este file
+- Seguem-se algumas queries de consulta a usar no engine:
+
+    funções úteis:  - db.collection.findOne(query, projection)
+                    - (...)
+
 '''
-import json
-import pymongo  # pip install pymongo
-from bson import json_util 
-from pymongo import MongoClient # Comes with pymongo
 
-conn = pymongo.Connection('mongodb://user:user123@localhost27017.mongolab.com:33499/enron')
-client = MongoClient('localhost', 27017)
-db = client.lei
-collection.find_one({},{"goodPerformance": 1}) 
+import pymongo
+# pprint library is used to make the output look more pretty
+from pprint import pprint
 
+# Connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["lei"]
+mycol = mydb["dialogs"]
 
+x = mycol.find_one()
 
+y = mycol.find_one(
+    { },
+    { "greetingsI": 1 }
+)
+
+pprint(x) 
+pprint(y)
